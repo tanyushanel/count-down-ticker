78f7059a50b5c2125c38b59038b8b0ba7d55ab28
tanyushanel <tanyushanel@users.noreply.github.com>
2020-09-24 12:56:04 +0300

    directives ngIf/switch added

--- src/app/time-picker/time-picker.component.ts
+++ src/app/time-picker/time-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-time-picker',
@@ -6,8 +6,26 @@ import { Component, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['./time-picker.component.scss'],
 })
 export class TimePickerComponent implements OnInit {
-  @Input() date: Date;
+  @Input() date: Date = new Date();
+  now = new Date();
+
+  get diff(): number {
+    return this.date.valueOf() - this.now.valueOf();
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  get restDays(): number {
+    return Math.floor(Math.abs(this.diff) / 1000 / 3600 / 24);
+  }
+
+  get restHours(): number {
+    return Math.floor(Math.abs(this.diff) / 1000 / 3600);
+  }
+
+  get restMinutes(): number {
+    return Math.floor(Math.abs(this.diff) / 1000 / 60);
+  }
 }
