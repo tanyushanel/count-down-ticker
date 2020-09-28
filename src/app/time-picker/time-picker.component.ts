6a86b5967ff031dfad6f0b760595d4ade7209d40
tanyushanel <tanyushanel@users.noreply.github.com>
2020-09-28 17:19:18 +0300

    time-down-couter added

--- src/app/time-picker/time-picker.component.ts
+++ src/app/time-picker/time-picker.component.ts
@@ -1,31 +1,40 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-time-picker',
   templateUrl: './time-picker.component.html',
   styleUrls: ['./time-picker.component.scss'],
 })
-export class TimePickerComponent implements OnInit {
-  @Input() date: Date = new Date();
-  now = new Date();
+export class TimePickerComponent {
+  date: Date = new Date();
 
-  get diff(): number {
-    return this.date.valueOf() - this.now.valueOf();
-  }
+  now = new Date();
 
   constructor() {}
 
-  ngOnInit(): void {}
+  get diff(): number {
+    return (this.date.valueOf() - this.now.valueOf()) / 1000;
+  }
 
   get restDays(): number {
-    return Math.floor(Math.abs(this.diff) / 1000 / 3600 / 24);
+    return Math.floor(Math.abs(this.diff) / 3600 / 24);
   }
 
   get restHours(): number {
-    return Math.floor(Math.abs(this.diff) / 1000 / 3600);
+    return Math.floor((Math.abs(this.diff) / 3600) % 24);
   }
 
   get restMinutes(): number {
-    return Math.floor(Math.abs(this.diff) / 1000 / 60);
+    return Math.floor((Math.abs(this.diff) / 60) % 60);
+  }
+
+  get restSeconds(): number {
+    return Math.floor(Math.abs(this.diff) % 60);
+  }
+
+  onDateChanged(event): void {
+    setInterval(() => {
+      this.now = new Date();
+    }, 1000);
   }
 }
